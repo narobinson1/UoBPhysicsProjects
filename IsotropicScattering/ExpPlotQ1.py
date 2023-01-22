a0a1b8209ffae3702d28dd8ff676322e85b220ea
nrobinson <67024254+narobinson1@users.noreply.github.com>
2023-01-22 17:02:14 +0100

    Update ExpPlotQ1.py

--- IsotropicScattering/ExpPlotQ1.py
+++ IsotropicScattering/ExpPlotQ1.py
@@ -5,6 +5,7 @@ Created on Sat Mar 26 13:01:40 2022
 
 @author: nrobinson15
 """
+
 import matplotlib.pyplot as plt
 import numpy as np
 from math import exp
@@ -22,22 +23,11 @@ def func1(text_file):
         y = float(lines[i])
         bins.append(y/800)
         accurate_exp.append(exp(-bins_index[i]))
-        
-
 
 func1("CumulativeMethodData.txt")
-
-
-
-
-
 plt.plot(bins_index, bins, 'o', ms=4, label='Cumulative Method')
 plt.plot(bins_index, accurate_exp, label='$e^{-τ}$')
-
-
 plt.xlabel("τ")
 plt.ylabel("$e^{-τ}$")
 plt.legend(prop={'size': 9})
-
-
 plt.savefig("ExpTauQ1")
